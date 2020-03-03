e013de4e7053da8cce8e2efe8eedca41d5b6c930
MatheusMaxRabelo <MatheusMaxRabelo@users.noreply.github.com>
2020-03-03 17:28:28 -0300

    m

--- GettingReady/GettingReady/Model/Turma.cs
+++ GettingReady/GettingReady/Model/Turma.cs
@@ -8,7 +8,16 @@ namespace GettingReady.Model
 {
     public class Turma : Entidade
     {
+<<<<<<< HEAD
         public int DisciplinaId { get; set; }
         public int turmaId { get; set; }
+=======
+<<<<<<< Updated upstream
+=======
+        public int DisciplinaId { get; set; }
+        public int turmaId { get; set; }
+>>>>>>> Stashed changes
+        
+>>>>>>> Matheus
     }
 }
