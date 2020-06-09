abc6d6cb403b2a62c613cea9475abd72ea3cdeeb
Merizito <juninho.meritus@hotmail.com>
2020-06-08 22:40:42 -0300

    Adicionada Tela Details de Professor/Questoes.

---
+++ GettingReady/Pages/user_Professor/Questoes/Details.razor.cs
@@ -0,0 +1,37 @@
+﻿using GettingReady.Model;
+using GettingReady.ViewModel.Admin;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReady.Pages.user_Professor.Questoes
+{
+    public class DetailsBase : ComponentBase
+    {
+        [Parameter]
+        public int QuestaoId { get; set; }
+        public QuestaoData Questao { get; set; }
+        [Inject]
+        public HttpClient Client { get; set; }
+        public bool isLoading { get; set; } = true;
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Load();
+        }
+        protected async Task Load()
+        {
+            Questao = new QuestaoData();
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://trabalhocleber.azurewebsites.net");
+            Questao.QuestaoInfo = await Client.GetJsonAsync<Questao>($"api/Questoes/{QuestaoId}");
+            Questao.Assuntos = await Client.GetJsonAsync<Assunto>($"api/Assuntos/{Questao.QuestaoInfo.AssuntoId}");
+            Questao.Professores = await Client.GetJsonAsync<Professor>($"api/Professores/{Questao.QuestaoInfo.ProfessorId}");
+        }
+    }
+}
---
+++ GettingReady/ViewModel/Admin/QuestaoData.cs
@@ -0,0 +1,16 @@
+﻿using GettingReady.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReady.ViewModel.Admin
+{
+    public class QuestaoData
+    {
+        public Questao QuestaoInfo { get; set; }
+        public Assunto Assuntos { get; set; }
+        public Professor Professores { get; set; }
+    }
+}
